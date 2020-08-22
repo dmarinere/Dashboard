65b936a449b2c913b1c45b7ad64c133060705899
Osuolale Emmanuel <59930570+dmarinere@users.noreply.github.com>
2020-08-22 14:06:27 +0100

    Update app.py

--- app.py
+++ app.py
@@ -6,8 +6,6 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 
-from sklearn.model_selection import train_test_split
-
 
 def main():
 
@@ -17,11 +15,10 @@ def main():
 
 	
     if page == 'Homepage':
-			 	st.title('A brief Data Description  ⓿ ➀ ➁ ➂ 🔢 ')
-        st.write("""This data would show the people who used our service in different stores, we would sample some rows to show 
-				what the data is about but basically we have data that shows""")
+	st.title('A brief Data Description ')
+        st.write("""This is a sample of our store data that has been collected already this give an idea of the data""")
         train = pd.merge(train, store, on='Store')
-				test = pd.merge(test, store, on='Store')
+	test = pd.merge(test, store, on='Store')
         st.dataframe(train.sample(20))
         st.text('Select a page in the sidebar')
 				
@@ -36,7 +33,7 @@ def main():
     else:
         st.title('Modelling')
         st.markdown('### Make prediction')
-				df = pd.read_csv("https://iyanu2.blob.core.windows.net/unzipped/prediction.csv")
+	df = pd.read_csv("https://iyanu2.blob.core.windows.net/unzipped/prediction.csv")
         st.dataframe(df)
         row_number = st.number_input('Select Store', min_value=1, max_value=len(df['Store'].nunique()-1), value=0)
 				sub = df[df['Store']==row_number]
@@ -44,17 +41,17 @@ def main():
         end_date = st.sidebar.date_input('end date', datetime.date(2015,9,20))
         days = (sub['Date'] > start_date) & (sub['Date'] <= end_date)
         st.markdown('#### Predicted Sales')
-				dis = sub.loc[days]
-				st.write(dis)
+	dis = sub.loc[days]
+	st.write(dis)
 
 
 
 @st.cache
 def load_data():
-    train= pd.read_csv("https://iyanu2.blob.core.windows.net/unzipped/rossmann-store-sales/train.csv"),
-		test= pd.read_csv("https://iyanu2.blob.core.windows.net/unzipped/rossmann-store-sales/test.csv"), 
-		store= pd.read_csv("https://iyanu2.blob.core.windows.net/unzipped/rossmann-store-sales/store.csv")
-		return train, test, store
+	train= pd.read_csv("https://iyanu2.blob.core.windows.net/unzipped/rossmann-store-sales/train.csv"),
+	test= pd.read_csv("https://iyanu2.blob.core.windows.net/unzipped/rossmann-store-sales/test.csv"), 
+	store= pd.read_csv("https://iyanu2.blob.core.windows.net/unzipped/rossmann-store-sales/store.csv")\
+	return train, test, store
 
 if __name__ == '__main__':
     main()
